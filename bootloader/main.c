e9e7f8c2de13c5b17d05c155750721160c1c6958
benflo <benji@DESKTOP-JQVMOHH.localdomain>
2026-03-07 12:27:37 +0100

    working bare metal bootloader

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,26 +1,11 @@
 #include <stdint.h>
-#include "hardware/gpio.h"
-#include "pico/time.h"
 
-#define LED_PIN         15
-#define FIRMWARE_BASE   0x10040000
+#define FIRMWARE_BASE 0x10040000
 
-int main() {
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    /* 3 blinkar = bootloader kör */
-    for (int i = 0; i < 3; i++) {
-        gpio_put(LED_PIN, 1); sleep_ms(200);
-        gpio_put(LED_PIN, 0); sleep_ms(200);
-    }
-    sleep_ms(300);
-
-    /* Läs SP och reset-handler från firmware vektortabell */
+int main(void) {
     uint32_t sp  = *(volatile uint32_t *)(FIRMWARE_BASE + 0);
     uint32_t rst = *(volatile uint32_t *)(FIRMWARE_BASE + 4);
 
-    /* Hoppa till firmware */
     __asm__ volatile (
         "msr msp, %0\n"
         "bx  %1\n"
