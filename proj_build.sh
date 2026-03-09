760be26b86d01a348ef4bbb85cdd7b2f971cb99a
benflo <benji@DESKTOP-JQVMOHH.localdomain>
2026-03-09 17:39:51 +0100

    fixed build script

--- proj_build.sh
+++ proj_build.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-
 set -e
 rm -rf build
+mkdir -p uf2
+
+echo "===================================================" 
+echo "===================================================" 
+echo "============= Building Bootloader... =============="
+echo "===================================================" 
+echo "===================================================" 
+cd bootloader
+make clean
+make
+cp bootloader.uf2 ../uf2
+cd ..
+
+echo "===================================================" 
+echo "===================================================" 
+echo "============== Building Firmware... ==============="
+echo "===================================================" 
+echo "===================================================" 
+
 PICO_SDK_PATH=/home/benji/pico-sdk cmake -B build
 cmake --build build
-mkdir -p uf2
-cp build/bootloader/bootloader.uf2 uf2/
+
 cp build/firmware/firmware.uf2 uf2/
-echo "UF2 files copied to uf2/"
