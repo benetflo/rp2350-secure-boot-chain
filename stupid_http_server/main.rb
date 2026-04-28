d5833e31156d350929a4183d3b05f9d6df8fc192
benflo <benji@DESKTOP-JQVMOHH.localdomain>
2026-04-28 21:29:05 +0200

    removed comment

--- stupid_http_server/main.rb
+++ stupid_http_server/main.rb
@@ -3,13 +3,12 @@ require 'sinatra'
 set :bind, '0.0.0.0'
 set :port, 4567
 
-# ÄNDRA DEN HÄR FÖR ATT TESTA VERSIONER
-VERSION = 0   # <-- ändra till 1, 2, 3 osv
+# Change this to test versions
+VERSION = 1
 
 get '/firmware' do
     partition = params['partition'].to_i
 
-    # Bygg filnamnet baserat på VERSION
     if partition == 0
         fw_path = "firmware/firmware_b_v#{VERSION}.bin"
     else
@@ -18,7 +17,7 @@ get '/firmware' do
 
     fw = File.binread(fw_path)
 
-    # Skicka version först (4 bytes little endian)
+    # Send version first (4 bytes little endian)
     header = [VERSION].pack("V")
 
     content_type 'application/octet-stream'
